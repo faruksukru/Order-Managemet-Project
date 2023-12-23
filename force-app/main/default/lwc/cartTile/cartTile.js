ba8597d04f0d15e79a9fe0ef04dcf6dea2564222
fatihemrenur <fatihemrenur@gmail.com>
2023-12-23 18:58:57 +0300

    commit 23122023v_2

--- force-app/main/default/lwc/cartTile/cartTile.js
+++ force-app/main/default/lwc/cartTile/cartTile.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getcartItems from '@salesforce/apex/ProductDetailsController.getCardItems';
+export default class CartTile extends LightningElement {
 
-export default class CartTile extends LightningElement {}
+    @api recordId;
+    productId;
+allCartItems=[];
+
+    @wire(getcartItems, { cartId: '$recordId'}) 
+    cartItems({data,error}){ // created a function, function name can be anything
+    if(data){ //if we retrieve  data
+    this.allCartItems = data;//assign all similar products to totalRecords
+    this.productId=data.product__c;
+    }else if(error){//if we retrieve  data
+        alert('There is an Error')
+    this.allCartItems=undefined;
+    }};
+
+}
