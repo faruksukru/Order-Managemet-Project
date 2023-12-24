b062248457c346a9761a8cb173aa63809d256b78
fatihemrenur <fatihemrenur@gmail.com>
2023-12-24 12:29:53 +0300

    commit 24122923v_2

--- force-app/main/default/lwc/cartTile/cartTile.js
+++ force-app/main/default/lwc/cartTile/cartTile.js
@@ -1,19 +1,39 @@
 import { LightningElement, api, wire } from 'lwc';
-import getcartItems from '@salesforce/apex/ProductDetailsController.getCardItems';
+import getcartItems from '@salesforce/apex/ProductDetailsController.getCartItems';
+import getAllCartProducts from '@salesforce/apex/ProductCategoriesController.getAllProduct';
 export default class CartTile extends LightningElement {
 
     @api recordId;
-    productId;
-allCartItems=[];
+    productIds =[];
+    allCartItems=[];
+    allProducts=[];
+
 
     @wire(getcartItems, { cartId: '$recordId'}) 
     cartItems({data,error}){ // created a function, function name can be anything
     if(data){ //if we retrieve  data
     this.allCartItems = data;//assign all similar products to totalRecords
-    this.productId=data.product__c;
+    data.forEach(element => {
+        this.productIds.push(element.Product__c);
+    });
+    console.log('Test'+ JSON.stringify(this.allCartItems));
     }else if(error){//if we retrieve  data
         alert('There is an Error')
     this.allCartItems=undefined;
     }};
+
+@wire(getAllCartProducts, { productIds: '$productIds'}) 
+allProduct ({data,error}){ // created a function, function name can be anything
+    if(data){ //if we retrieve  data
+    this.allProducts = data;//assign all similar products to totalRecords
+        console.log('Test'+ JSON.stringify(this.allProducts));
+    }else if(error){//if we retrieve  data
+        alert('There is an Error')
+    this.allProducts=undefined;
+    }};
+
+    relatedImage(){
+
+    }
 
 }
